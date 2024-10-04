22e97406197be8b6843438c6d551c772b92df9e3
Antônio Rocha <antoniojunior159@gmail.com>
2024-10-03 21:35:37 -0300

    Create Applications

---
+++ ProducerConsumerRabbitMQ/Consumer/Receiver.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace Producer;
+public class Receiver
+{
+    public static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        using (var chanel = connection.CreateModel())
+        {
+            chanel.QueueDeclare("BasicTest", false, false, false, null);
+            
+            var consumer = new EventingBasicConsumer(chanel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($"Received message {message}...");
+            };
+
+            chanel.BasicConsume("BasicTest", true, consumer);
+
+            Console.WriteLine("Press [Ente] to exit the Receiver App...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ProducerConsumerRabbitMQ/Producer/Sender.cs
@@ -0,0 +1,26 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace Producer;
+public class Sender
+{ 
+    public static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection()) 
+        using (var chanel = connection.CreateModel())
+        {
+            chanel.QueueDeclare("BasicTest", false, false, false, null);
+
+            string message = "Getting Started with .NET RabbitMQ";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            chanel.BasicPublish("", "BasicTest", null, body);
+
+            Console.WriteLine($"Send message {message}...");
+        }
+
+        Console.WriteLine("Press [Ente] to exit the Sender App...");
+        Console.ReadLine();
+    }
+}
